b3c0076e1ad060492b70588a6b3bb4a3181c6ec5
Ceres <cagutierrezpayares@gmail.com>
2026-05-29 19:12:35 -0500

    ups

--- src/main/java/dev/cerez/command/commands/Register.java
+++ src/main/java/dev/cerez/command/commands/Register.java
@@ -3,6 +3,8 @@ package dev.cerez.command.commands;
 import dev.cerez.Main;
 import dev.cerez.MedicalFormula;
 import dev.cerez.command.BaseCommand;
+import dev.cerez.command.Category;
+import dev.cerez.command.Status;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -10,16 +12,14 @@ import java.util.List;
 public class Register extends BaseCommand {
     @Override
     public void execute(@NotNull List<String> arguments) {
-        if (arguments.size() >= 2) {
-            MedicalFormula medicalFormula = new MedicalFormula(arguments.get(0), Double.parseDouble(arguments.get(1)));
+        if (arguments.size() >= 3) {
+            MedicalFormula medicalFormula = new MedicalFormula(arguments.get(0), Status.valueOf(arguments.get(1)), Category.valueOf(arguments.get(2)));
             Main.list.add(medicalFormula);
             Main.map.put(medicalFormula.codeFormula(), medicalFormula);
             Main.queue.add(medicalFormula);
             System.out.println("ok");
-        }else if  (arguments.size() == 1) {
-            System.err.println("Faltas el argumentos de tamaño: add <Nombre> <Cantidad>");
         }else {
-            System.err.println("Faltas el argumentos de nombre y tamaño: add <Nombre> <Cantidad>");
+            System.err.println("Faltas el argumentos: add <Nombre> <Cantidad>");
         }
 
     }
